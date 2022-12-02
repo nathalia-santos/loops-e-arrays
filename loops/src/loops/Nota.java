6bc0e6db7ea7c01e2ad863a03e25a7453320e22a
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-12-02 10:27:28 -0300

    ex2

---
+++ loops/src/loops/Nota.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Nota {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int nota;
+		
+		System.out.println("Nota: ");
+		nota = scan.nextInt();
+		
+		while(nota < 0 || nota > 10) {
+			System.out.println("Nota inválida! Digite um valor entre 0 e 10!");
+			nota = scan.nextInt();
+		}
+		
+		
+	}
+
+}
