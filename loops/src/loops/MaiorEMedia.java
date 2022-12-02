ff5b6d697121c57b4814d288139d7fa48af9d422
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-12-02 13:22:02 -0300

    ex3

--- loops/src/loops/MaiorEMedia.java
+++ loops/src/loops/MaiorEMedia.java
@@ -8,16 +8,20 @@ public class MaiorEMedia {
 		
 		int numero;
 		int maior = 0;
+		int soma = 0;
 		int cont = 0;
 		
 		do {
 			System.out.println("Número: ");
 			numero = scan.nextInt();
 			cont = cont + 1;
+			soma += numero;
 			if (numero > maior) {
 				maior = numero;
 			}
 		}while(cont < 5);
+		
 		System.out.println(maior);
+		System.out.println(soma/5);
 	}
 }
